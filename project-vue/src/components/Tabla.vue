1ca5278096e2798aaf5ac3b3b99a5592ba8b6385
rubentxogg <rubentxogg@hotmail.com>
2022-02-05 17:35:29 +0100

    [MOD] Estilos -> tabla

--- project-vue/src/components/Tabla.vue
+++ project-vue/src/components/Tabla.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="tabla">
-    <table class="table table-striped table-hover">
+    <table class="table table-hover table-striped">
       <thead>
         <tr>
-          <th>Pos</th>
+          <th></th>
+          <th colspan="2">Pos</th>
           <th>Club</th>
           <th>Pts</th>
         </tr>
@@ -11,7 +12,8 @@
 
       <tbody>
         <tr v-for="(equipo, index) in equipos" :key="equipo.id">
-          <th>{{ index + 1 }}</th>
+          <td :class="asignarColorPosicion(index + 1)"></td>
+          <th colspan="2">{{ index + 1 }}</th>
           <td><img :src="escudos[equipo.id]" width="24" height="24" class="me-1 mb-1"/> {{ equipo.name }}</td>
           <td>{{ equipo.points }}</td>
         </tr>
@@ -24,6 +26,23 @@
 export default {
   name: "Tabla",
   props: ["equipos", "escudos"],
+  data() {
+    return {
+      colorPosicion: {
+        descenso: "table-danger rounded-3",
+        champions: "table-primary rounded-3",
+        uefa: "table-warning rounded-3"
+      }
+    }
+  },
+  methods: {
+    asignarColorPosicion(index) {
+      if(index > 17 && index < 21) return this.colorPosicion.descenso;
+      if(index > 0 && index < 5) return this.colorPosicion.champions;
+      if(index > 4 && index < 7) return this.colorPosicion.uefa;
+      return "";
+    }
+  }
 };
 </script>
 
