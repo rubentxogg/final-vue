38652c16c553e075490b66b9289aeb006b3f2160
rubentxogg <rubentxogg@hotmail.com>
2022-02-06 22:17:17 +0100

    [ADD] components -> DesplegableJornadas

--- project-vue/src/components/DesplegableJornadas.vue
+++ project-vue/src/components/DesplegableJornadas.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="desplegable-jornadas mb-auto text-center">
-    <button class="btn btn-dark btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+  <div class="desplegable-jornadas mt-3 text-center">
+    <button class="btn btn-dark dropdown-toggle fs-4" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false" @click="mostrarJornadas">
       Seleccionar jornada
     </button>
 
-    <ul class="dropdown-menu" arial-labelledby="dropdownMenuButton">
-      <li class="dropdown-item" v-for="jornada in jornadas" :key="jornada.id">
-        <i class="bi bi-calendar4 me-1"></i> {{ jornada.round }} - {{ jornada.date }}
+    <ul v-if="jornadas.length > 0" class="dropdown-menu" arial-labelledby="dropdownMenuButton">
+      <li class="dropdown-item text-center p-2" v-for="jornada in jornadas" :key="jornada.id" @click="partidosJornada(jornada.round, jornada.date)">
+        <i class="bi bi-calendar4 me-1 mb-1"></i> {{ jornada.round }} - {{ jornada.date }}
         <hr class="dropdown-divider" />
       </li>
     </ul>
+
+    <h2 v-else class="text-center mt-5">No hay jornadas</h2>
   </div>
 </template>
 
@@ -17,6 +19,15 @@
 export default {
   name: "DesplegableJornadas",
   props: ["jornadas"],
+  events: ["mostrarJornadas","partidosJornada"],
+  methods: {
+    partidosJornada(jornada, fecha) {
+      this.$emit("partidosJornada", jornada, fecha);
+    },
+    mostrarJornadas() {
+      this.$emit("mostrarJornadas");
+    }
+  }
 };
 </script>
 
@@ -26,6 +37,10 @@ li {
 }
 
 .desplegable-jornadas {
+  width: 100%;
+}
+
+h2 {
   width: 100%;
 }
 </style>
--- project-vue/src/views/Jornadas.vue
+++ project-vue/src/views/Jornadas.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="jornadas">
+  <div class="jornadas d-flex align-self-start">
     <h1 class="text-center mt-4"><i class="bi bi-calendar-week m-3"></i>Jornadas</h1>
-    <hr class="w-75 mb-5" />
-
-    <desplegable-jornadas :jornadas="jornadas" />
+    <hr class="w-75 mb-auto" />
+    <h2 v-if="isLoading" class="text-center">Cargando...</h2>
+    <h2 v-else-if="jornadas.lenth < 1" class="text-center">No hay jornadas para mostrar</h2>
+    <desplegable-jornadas v-else :jornadas="jornadas" @partidosJornada="partidosEnJornada" @mostrarJornadas="mostrarJornadas"/>
   </div>
 </template>
 
@@ -18,20 +19,31 @@ export default {
   },
   data() {
     return {
-      jornadas: []
+      jornadas: [],
+      partidosPorJornada: [],
+      isLoading: false
     }
   },
   methods: {
     getJornadas(URL) {
+      this.isLoading = true;
       axios
         .get(URL)
         .then((response) => (this.jornadas = response.data))
+        .then(() => (this.jornadas = [...new Map(this.jornadas.map(jornada => [jornada.round && jornada.date, jornada])).values()]))
         .catch((error) => console.error(error))
+        .finally(() => (this.isLoading = false));
+    },
+    partidosEnJornada(jornada, fecha) {
+      console.log(`${jornada} - ${fecha}`);
+    },
+    mostrarJornadas() {
+     // TODO
     }
   },
   mounted() {
     this.getJornadas('http://localhost:3000/matches');
-  }
+  },
 };
 </script>
 
