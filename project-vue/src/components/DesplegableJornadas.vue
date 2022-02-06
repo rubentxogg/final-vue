42fd1f179d58fc6ffaccfc6a3eca4b55d3d3e520
rubentxogg <rubentxogg@hotmail.com>
2022-02-06 13:44:13 +0100

    [ADD] Component -> DesplegableJornadas *Sin acabar

--- project-vue/src/components/DesplegableJornadas.vue
+++ project-vue/src/components/DesplegableJornadas.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="desplegable-jornadas">
-      <h1>Desplegable Jornadas</h1>
+  <div class="desplegable-jornadas mb-auto text-center">
+    <button class="btn btn-dark btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+      Seleccionar jornada
+    </button>
+
+    <ul class="dropdown-menu" arial-labelledby="dropdownMenuButton">
+      <li class="dropdown-item" v-for="jornada in jornadas" :key="jornada.id">
+        <i class="bi bi-calendar4 me-1"></i> {{ jornada.round }} - {{ jornada.date }}
+        <hr class="dropdown-divider" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-    name: "DesplegableJornadas"
-}
+  name: "DesplegableJornadas",
+  props: ["jornadas"],
+};
 </script>
 
-<style>
+<style scoped>
+li {
+  cursor: pointer;
+}
 
+.desplegable-jornadas {
+  width: 100%;
+}
 </style>
--- project-vue/src/views/Jornadas.vue
+++ project-vue/src/views/Jornadas.vue
@@ -3,7 +3,7 @@
     <h1 class="text-center mt-4"><i class="bi bi-calendar-week m-3"></i>Jornadas</h1>
     <hr class="w-75 mb-5" />
 
-    <desplegable-jornadas />
+    <desplegable-jornadas :jornadas="jornadas" />
   </div>
 </template>
 
@@ -28,6 +28,9 @@ export default {
         .then((response) => (this.jornadas = response.data))
         .catch((error) => console.error(error))
     }
+  },
+  mounted() {
+    this.getJornadas('http://localhost:3000/matches');
   }
 };
 </script>
