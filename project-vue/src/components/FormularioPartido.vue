bb8fa2dc84f218eeda67e0c00a0b4de4647f425b
rubentxogg <rubentxogg@hotmail.com>
2022-02-08 18:36:23 +0100

    [ADD] components -> Formulario layout

--- project-vue/src/components/FormularioPartido.vue
+++ project-vue/src/components/FormularioPartido.vue
@@ -1,6 +1,6 @@
 <template>
   <form class="formulario-partido mb-auto">
-    <fieldset class="border border-4 rounded p-5 pt-0">
+    <fieldset class="border border-4 rounded p-5 pt-0 d-flex flex-wrap flex-column">
       <legend class="text-muted text-center fs-4 mb-4 mt-2">Añadir partido</legend>
 
     <p class="d-flex">
@@ -17,6 +17,15 @@
       <desplegable-equipos :escudos="escudos" :equipos="equipos" @seleccionEquipo="seleccionarEquipo2"/>
       <input class="text-center fs-5" type="text" name="jornada" readonly :value="equipo2"/>
     </p>
+
+    <p class="d-flex">
+      <button class="btn btn-secondary ms-4" type="button">
+        <i class="bi bi-clock-history"></i> Fecha&nbsp;&nbsp;&nbsp;
+      </button>
+      <input class="m-auto fs-5 text-center ms-5" type="date" name="fecha" v-model="fecha">
+    </p>
+
+    <button type="button" :class="desactivarBoton">Añadir</button>
     
     </fieldset>
   </form>
@@ -40,16 +49,17 @@ export default {
         equipos: [],
         nombreJornada: "",
         equipo1: "",
-        equipo2: ""
+        equipo2: "",
+        fecha: "",
     }
   },
   methods: {
     getJornadas(URL) {
-    axios
-     .get(URL)
-     .then((response) => (this.jornadas = response.data))
-     .then(() => (this.jornadas = [...new Map(this.jornadas.map(jornada => [jornada.round, jornada])).values()]))
-     .catch((error) => console.error(error));
+      axios
+        .get(URL)
+        .then((response) => (this.jornadas = response.data))
+        .then(() => (this.jornadas = [...new Map(this.jornadas.map(jornada => [jornada.round, jornada])).values()]))
+        .catch((error) => console.error(error));
     },
     getEquipos(URL) {
       axios
@@ -68,6 +78,12 @@ export default {
       this.equipo2 = equipo;
     }
   },
+  computed: {
+    desactivarBoton() {
+      if(this.equipo1 === "" || this.equipo2 === "" || this.fecha === "" || this.nombreJornada === "") return "btn btn-lg mt-4 w-75 m-auto btn-danger disabled";
+      return "btn btn-lg mt-4 w-75 m-auto btn-dark";
+    }
+  },
   mounted() {
     this.getJornadas('http://localhost:3000/matches');
     this.getEquipos("http://localhost:3000/clubs");
@@ -83,5 +99,10 @@ li {
 input{
   border: 0;
   border-bottom: 2px solid rgba(0, 0, 0, 0.705);
+}
+
+input[type="date"] {
+  margin-left: 1rem;
+  width: 250px;
 }
 </style>
--- project-vue/src/views/NuevoPartido.vue
+++ project-vue/src/views/NuevoPartido.vue
@@ -15,7 +15,7 @@ export default {
   props: ["escudos"],
   components : {
     FormularioPartido
-  }
+  },
 };
 </script>
 
