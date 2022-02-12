3d83d0add5b136d511cb5172fe5b1f6b71a93666
rubentxogg <rubentxogg@hotmail.com>
2022-02-12 13:33:20 +0100

    [ADD] Components -> CardeEquipo *Frases equipos

--- project-vue/src/components/CardEquipo.vue
+++ project-vue/src/components/CardEquipo.vue
@@ -6,14 +6,12 @@
 
     <div class="card-body">
       <h5 class="card-title">{{ equipo.name }}</h5>
-      <p class="card-text">
-        With supporting text below as a natural lead-in to additional content.
-      </p>
+      <p class="card-text">{{ fraseEquipo(equipo.id) }}</p>
       
       <button class="btn btn-outline-secondary" @click="getJugadores(equipo.name)">{{ mostrarOcultarJugadores }}</button>
       <tabla-jugadores class="mt-3" :jugadores="jugadores" v-if="jugadores.length > 0 & showJugadores" />
 
-      <button :class="botonNuevoJugador" @click="mostrarFormularioNuevoJugador">
+      <button :class="botonNuevoJugador" @click="mostrarFormularioNuevoJugador" class="mb-2 ms-3">
         <i class="bi bi-person-plus m-1"></i>{{ textoBotonFormularioNuevoJugador }}
       </button>
 
@@ -41,7 +39,29 @@ export default {
       showJugadores: false,
       showNuevoJugador: false,
       nombreEquipo: "",
-      jugadores: []
+      jugadores: [],
+      frasesEquipo: [
+        "Bihotz armagina",
+        "O fogar da afouteza, orgullo e tradición",
+        "Con la luz y la magia de la Alhambra y la Sierra Nevada y su blanco resplandor",
+        "Zu zara nagusia",
+        "Ese Cádiz, OÉ ",
+        "Fuerte y rojo es el color de tu blusa y tu bandera",
+        "Més que un club",
+        "Mucho elche, mucho elche",
+        "Hasta el final, vamos Real",
+        "Vamos Geta",
+        "Gure hiria, Gure Taldeak",
+        "Hay una leyenda que recorre el mundo entero",
+        "Con grandes triunfos se hace la historia",
+        "Txuri urdin maitea",
+        "Endavant",
+        "¡HUESCA! ¡HUESCA!",
+        "Che que bo",
+        "Tu nombre, mi ideal",
+        "Nunca dejes de creer",
+        "Dicen que nunca se rinde"
+      ]
     }
   },
   methods : {
@@ -69,6 +89,9 @@ export default {
       } else {
         this.showJugadores= !this.showJugadores;
       }
+    },
+    fraseEquipo(equipo) {
+      return this.frasesEquipo[equipo];
     }
   },
   computed: {
@@ -83,7 +106,7 @@ export default {
     botonNuevoJugador() {
       if(!this.showNuevoJugador) return "btn btn-outline-success mt-2";
       return "btn btn-outline-warning mt-2"
-    }
+    },
   }
 };
 </script>
@@ -91,5 +114,9 @@ export default {
 <style scoped>
 .form-jugador {
   margin-top: -2rem;
+}
+
+.equipos {
+  width: 33rem;
 }
 </style>
