73de97aa40bde028c0da7e58ecdcdd43751388d8
rubentxogg <rubentxogg@hotmail.com>
2022-02-15 22:03:02 +0100

    [MOD] JumbotronInicio - h1 Width

--- project-vue/src/components/JumbotronInicio.vue
+++ project-vue/src/components/JumbotronInicio.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="jumbotron-inicio text-center">
       <img :src="logo" alt="laLigaLogo" class="img-fluid">
-        <h1 class="display-5 fw-bold">Temporada 2021-2022</h1>
+        <h1 class="display-5 fw-bold w-100">Temporada 2021-2022</h1>
         <p class="col-md-8 fs-4 w-100">
           Vive el fútbol, vive La Liga.
         </p>
